049aa3a6064d484ba8415e120dda3d6a0c07aeae
codeshuffle <bogdan.balasanu@gmail.com>
2020-08-26 15:45:29 -0700

    L197 - converters, command objects

--- src/main/java/guru/springframework/service/RecipeService.java
+++ src/main/java/guru/springframework/service/RecipeService.java
@@ -1,11 +1,14 @@
 package guru.springframework.service;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.Recipe;
-import org.springframework.data.repository.CrudRepository;
+
 
 import java.util.Set;
 
 public interface RecipeService {
     Set<Recipe> getRecipes();
+    Recipe findById(Long l);
 
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
